ea3241f2526a1bbf1319ac1c425a044ab336e842
arda <ardaklc0@gmail.com>
2024-01-04 16:59:57 +0300

    The virtual ide for business create has been created. Some cards (Create Entity, Show All Entities) have been created on the home page.

---
+++ WebUI/BussMaker.WebUI/Controllers/IDEController.cs
@@ -0,0 +1,20 @@
+﻿using BussMaker.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BussMaker.WebUI.Controllers
+{
+    public class IDEController : Controller
+    {
+        private readonly IEntityService service;
+        public IDEController(IEntityService service)
+        {
+            this.service = service;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var existingEntity = await service.GetEntityAsync(1);
+            ViewBag.Title = existingEntity.Name;
+            return View();
+        }
+    }
+}
